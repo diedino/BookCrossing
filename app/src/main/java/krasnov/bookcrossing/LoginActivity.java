08bf026a499a8671d3c533adbf34c900c6199510
Vasiliy <vvkrasnov@edu.hse.ru>
2019-03-22 23:23:54 +0300

    login screen design

--- app/src/main/java/krasnov/bookcrossing/LoginActivity.java
+++ app/src/main/java/krasnov/bookcrossing/LoginActivity.java
@@ -5,10 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class LoginActivity extends AppCompatActivity {
 
     private Button loginBtn;
+    private TextView signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +21,15 @@ public class LoginActivity extends AppCompatActivity {
 
         loginBtn = (Button) findViewById(R.id.btn_login);
 
+        signUp = (TextView) findViewById(R.id.link_signup);
+
         loginBtn.setOnClickListener(v -> {
             Intent myIntent = new Intent(this, BottomBarActivity.class);
             startActivity(myIntent);
+        });
+
+        signUp.setOnClickListener(v->{
+
         });
     }
 }
